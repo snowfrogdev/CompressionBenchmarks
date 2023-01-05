6d32959a5cd362a1a44059868db7c8d4da61327e
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2023-01-05 18:30:59 +0100

    Initial commit

---
+++ Compression.Benchmarks/Benchmarks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace Compression.Benchmarks;
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    [Benchmark]
+    public void Scenario1()
+    {
+        // Implement your benchmark here
+    }
+
+    [Benchmark]
+    public void Scenario2()
+    {
+        // Implement your benchmark here
+    }
+}
+
